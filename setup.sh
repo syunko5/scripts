1d84ead8c3aeafa84135c615cc660647ab37e2b9
syunko5 <syunko5@proton.me>
2026-02-22 19:07:21 -0300

    Removed Chaotic AUR

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ pkgsCore=(
     hyprland
     noctalia-shell
     hyprshot
+    swayimg
     xorg-xwayland
     xdg-desktop-portal-hyprland
     fish
@@ -41,23 +42,23 @@ preinstall() {
     sudo pacman -S --needed reflector --noconfirm
     sudo reflector -c Brazil,Argentina,Chile -a 12 -f 10 -p https --sort rate --save /etc/pacman.d/mirrorlist
 	sudo pacman -Syy --needed base-devel git stow --noconfirm
+
     sudo sed -i '38c\ParallelDownloads = 50' /etc/pacman.conf
-    
-	sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-	sudo pacman-key --lsign-key 3056513887B78AEB
-	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
-	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 	sudo sed -i '94c\[multilib]' /etc/pacman.conf
 	sudo sed -i '95c\Include = /etc/pacman.d/mirrorlist' /etc/pacman.conf
-	sudo sed -i '103c\[chaotic-aur]' /etc/pacman.conf
-	sudo sed -i '104c\Include = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
-
-	sudo pacman -S --needed yay --noconfirm
+	
+	cd ~
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si --noconfirm
+	cd ..
+	rm -rf yay
+	
 }
 
 dotfiles_setup() {
 	echo "[SETUP] Setting up dotfiles"
-    cd ~/
+    cd ~
 	git clone https://github.com/syunko5/dotfiles.git
 	cd dotfiles
 	stow .
